dfa4f5fd4f4385c415c61640d750784558c3e01d
Chironjit Chandra Roy <chironjitchandraroy@Chironjits-MacBook-Air.local>
2024-09-29 16:45:12 +0600

    add aplash screen

---
+++ lib/view/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:bn_en_trans/core/app_routes.dart';
+import 'package:bn_en_trans/utils/app_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 03), ()=>Get.offAllNamed(AppRoute.bottomNav));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      body: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15.r),
+            child: Image.asset(AppImage.appLogo, height: 200.h, width: 200.w,)),
+      ),
+
+    );
+  }
+}
